5fdac97cd8089771d098f0ecd0174bff376c9492
JustJason <tixtix133@gmail.com>
2026-06-17 23:39:36 +0800

    Implement 2D vector class with basic operations

---
+++ python до 9.02.py
@@ -0,0 +1,37 @@
+import math
+
+
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2D(self.x - other.x, self.y - other.y)
+
+    def __str__(self):
+        return f"Vector({self.x}, {self.y})"
+
+    def length(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+
+# Проверка работы программы
+
+v1 = Vector2D(3, 4)
+v2 = Vector2D(1, -2)
+
+print("Сложение:")
+print(v1 + v2)
+
+print("\nВычитание:")
+print(v1 - v2)
+
+print("\nДлина вектора:")
+print(v1.length())
+
+print("\nСтроковое представление:")
+print(v1)
