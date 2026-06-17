0602fa3f2df6fcc6142bb7ac60fcf50507861685
JustJason <tixtix133@gmail.com>
2026-06-18 00:55:46 +0800

    Update python до 30.03.py

--- python до 30.03.py
+++ python до 30.03.py
@@ -26,3 +26,21 @@ most_popular = max(data, key=data.get)
 
 print("Общее кол-во товаров:", total)
 print("Самый популярный товар:", most_popular)
+
+#задние 2
+with open("text.txt", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+
+word_count = 0
+longest_line = ""
+
+for line in lines:
+    words = line.split()
+    word_count += len(words)
+
+    if len(line) > len(longest_line):
+        longest_line = line
+
+print("Общее кол-во слов:", word_count)
+print("Общее кол-во строк:", len(lines))
+print("Самая длинная строка:", longest_line.strip())
